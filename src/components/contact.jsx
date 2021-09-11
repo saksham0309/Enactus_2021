fcb6c2359503a07778fab91f2f89cc39067969c3
Saksham Mishra <55156585+saksham0309@users.noreply.github.com>
2021-09-11 21:43:09 +0530

    Update contact.jsx

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -150,8 +150,8 @@ export const Contact = (props) => {
       <div id='footer'>
         <div className='container text-center'>
           <p>
-            &copy; 2020 Issaaf Kattan React Land Page Template. Design by{' '}
-            <a href='http://www.templatewire.com' rel='nofollow'>
+             Design by{' '}
+            <a href='https://saksham0309.github.io/Profile/#' rel='Saksham'>
               TemplateWire
             </a>
           </p>
